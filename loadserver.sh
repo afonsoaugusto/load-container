eca58613e427124f2ba7c712f0063d6bf661aa8d
Afonso Rodrigues <afonso.rodrigues@maxmilhas.com.br>
2020-08-24 10:10:31 -0300

    change memory value

--- loadserver.sh
+++ loadserver.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo $1
-/usr/bin/stress-ng --cpu 1 --vm 1 --vm-bytes 1024M --hdd 1 --fork 1 --switch 1 --timeout 600 --metrics &
+/usr/bin/stress-ng --cpu 1 --vm 1 --vm-bytes 500M --hdd 1 --fork 1 --switch 1 --timeout 600 --metrics &
